1a16514a5236f261219e0b470f5e1f63fb740bcf
itka0526 <itka0526@gmail.com>
2024-05-06 03:35:14 +0300

    NULL and nullptr?

---
+++ problems-in-cpp/94.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> ans;
+
+    void dfs(TreeNode *node) {
+        if (node != nullptr) {
+            dfs(node->left);
+            ans.push_back(node->val);
+            dfs(node->right);
+        }
+    };
+
+    vector<int> inorderTraversal(TreeNode *root) {
+        dfs(root);
+        return ans;
+    }
+};
